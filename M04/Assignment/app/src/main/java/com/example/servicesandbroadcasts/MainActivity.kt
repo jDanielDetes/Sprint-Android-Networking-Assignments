9ef4856b126cce8637e112ff41cbf75d859c23b7
jDanielDetes <jdanieldetes@gmail.com>
2019-09-05 18:04:27 -0400

    starting service

--- M04/Assignment/app/src/main/java/com/example/servicesandbroadcasts/MainActivity.kt
+++ M04/Assignment/app/src/main/java/com/example/servicesandbroadcasts/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.servicesandbroadcasts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            val serviceIntent = Intent(this,FileService::class.java)
+            this.startService(serviceIntent)
+        }
     }
+
 }
