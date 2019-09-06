a5b207375c7e744c5d8c7695babeac75c813555b
jDanielDetes <jdanieldetes@gmail.com>
2019-09-06 13:38:29 -0400

    working on recycler

--- Sprint/app/src/main/java/com/example/sprint5_pokemonapi/MainActivity.kt
+++ Sprint/app/src/main/java/com/example/sprint5_pokemonapi/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.sprint5_pokemonapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sprint5_pokemonapi.model.Pokemon
+import com.example.sprint5_pokemonapi.retro.Retrofit
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),retrofit2.Callback<Pokemon> {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_get_pkmn.setOnClickListener {
+            Retrofit.Showpkmn().enqueue(this)
+        }
     }
 }
