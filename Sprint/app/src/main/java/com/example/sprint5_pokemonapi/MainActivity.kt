2257a89df2d1634a6aa95a4aa66d0d29285aca67
jDanielDetes <jdanieldetes@gmail.com>
2019-09-06 11:19:05 -0400

    sprint

---
+++ Sprint/app/src/main/java/com/example/sprint5_pokemonapi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sprint5_pokemonapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
